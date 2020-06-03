5fb7901008757ee3be325e01e1db99b502fca944
Minjire <59723399+Minjire@users.noreply.github.com>
2020-06-03 10:07:17 +0300

    Update nsfw.py

--- nsfw.py
+++ nsfw.py
@@ -107,25 +107,6 @@ def main():
     # save model
     model.save("nsfw_model.h5")
 
-    # test for model accuracy after training
-    test_loss, test_acc = model.evaluate(test_images, test_labels)
-
-    print('\nTest accuracy:', test_acc)
-
-    # load model
-    model = keras.models.load_model("nsfw_model.h5")
-
-    prediction = model.predict(test_images)
-
-    # display the first 5 images and their predictions
-    plt.figure(figsize=(5, 5))
-    for i in range(5):
-        plt.grid(False)
-        plt.imshow(test_images[i], cmap=plt.cm.binary)
-        plt.xlabel("Actual: " + class_names[test_labels[i]])
-        plt.title("Prediction: " + class_names[np.argmax(prediction[i])])
-        plt.show()
-
 
 # %%
 main()
